66bb1c753809331a9aeb55461318854ddc2fb2c5
Crowdbotics <team@crowdbotics.com>
2021-12-02 01:14:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "horse_training_trac_32220",
+  url: "https://horse_training_trac_32220.botics.co",
+  api: "https://horse_training_trac_32220.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
